dcd8ce67c66f6715613c6905b12d99e5883f7f6b
Romaine De La Cruz <r2dcruz@gmail.com>
2021-01-30 19:47:09 -0800

    adding some skeletons

---
+++ app/js/practice.js
@@ -0,0 +1,18 @@
+/** 
+
+this will be all my practice js
+
+this file can be ignored, but I thought it would be fun to leave all my blogging here.
+
+1/30/2021
+* added all the directory and folders
+* cleaning up the paths
+
+**/
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
