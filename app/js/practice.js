ae65667679b41c79e1da8522f7192f4c6d24c6fc
Romaine De La Cruz <r2dcruz@gmail.com>
2021-01-31 22:11:55 -0800

    commit practice

--- app/js/practice.js
+++ app/js/practice.js
@@ -7,6 +7,8 @@ this file can be ignored, but I thought it would be fun to leave all my blogging
 1/30/2021
 * added all the directory and folders
 * cleaning up the paths
+* learning about landing page from freecodecamp.com https://www.youtube.com/watch?v=aoQ6S1a32j8&t=1340s
+* I plan to make a general webpage for myself, using the weather api
 
 **/
 
